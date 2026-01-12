a5dd7e0c14b7e5ccaaa6520d218bd9072a9b7fca
Melanie <git@melanie-busse.de>
2026-01-12 15:55:09 +0100

    dynamic character cards

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {createCharacterCard} from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,23 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchData() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log(data);
+
+    return data.results;
+
+}
+
+async function renderCharacters() {
+    const results = await fetchData();
+
+    results.forEach((result) => {
+        cardContainer.append(createCharacterCard(result));
+    });
+}
+
+renderCharacters();
+
