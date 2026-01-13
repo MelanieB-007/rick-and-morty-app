c87c25310bc6c74a8048d75d8b7cc786c891ad2e
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-01-13 08:41:32 +0100

    Task 4 - work in progress - Changed Code for Buttons and moved it to index.js

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -1,18 +1,35 @@
+// gehört zu index.js
+/*
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
+*/
 
+/*
 export function createNavButton(dataJs, buttonClass, name) {
   const button = document.createElement("button");
   button.innerHTML = `<button class="button ${buttonClass}" data-js="${dataJs}">
         ${name}
-      </button>`;
+      </button>`; // doppelter button?!
   return button;
 }
+  */
 
-prevButton.addEventlistener("click", () => {
-  page = currentPage - 1;
+export function createNavButton(dataJs, buttonClass, name) {
+  //erstelle einen leeren Button
+  const button = document.createElement("button"); // <button></button>
+  button.className = `button ${buttonClass}`; // <button class="button button--prev"></button
+  button.setAttribute("data-js", dataJs); // <button class="button button--prev" data-js="button-prev"></button>
+  button.textContent = name;
+  return button;
+}
+/*
+prevButton.addEventListener("click", () => {
+  page = page > 1 ? page - 1 : 1;
+  fetchCharacters();
 });
 
-nextButton.addEventlistener("click", () => {
-  page = currentPage + 1;
+nextButton.addEventListener("click", () => {
+  page = page < maxPage ? page + 1 : maxPage;
+  fetchCharacters();
 });
+*/
--- index.js
+++ index.js
@@ -12,13 +12,14 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
 
 async function fetchData() {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
+
   return data;
 }
 
@@ -34,6 +35,24 @@ async function renderButton(dataJs, buttonClass, name) {
 
 const results = await fetchData();
 renderCharacters();
+/*
 renderButton("button-prev", "button--prev", "prev");
 
 renderButton("button-next", "button--next", "next");
+*/
+
+const prevButton = createNavButton("button-prev", "button--prev", "prev");
+const nextButton = createNavButton("button-next", "button--next", "next");
+
+navigation.append(prevButton);
+navigation.append(nextButton);
+
+prevButton.addEventListener("click", () => {
+  page = page > 1 ? page - 1 : 1;
+  fetchCharacters();
+});
+
+nextButton.addEventListener("click", () => {
+  page = page < maxPage ? page + 1 : maxPage;
+  fetchCharacters();
+});
